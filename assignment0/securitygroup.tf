d0a70bf63f89af58d6e1466a7c849ef9d27407f1
EC2 Default User <ec2-user@ip-172-31-9-103.ap-south-1.compute.internal>
2020-05-09 15:25:25 +0000

    Notes

--- assignment0/securitygroup.tf
+++ assignment0/securitygroup.tf
@@ -27,24 +27,34 @@ resource "aws_security_group" "web-node" {
 
 }
 
-##resource "aws_security_group_rule" "MySql" {
-#  type              = "ingress"
-#  from_port         = "3306"
-#  to_port           = "3306"
-#  protocol          = "tcp"
-#  cidr_blocks       = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.web-node.id
-#
-#}
+resource "aws_security_group_rule" "MySql" {
+  type              = "ingress"
+  from_port         = "3306"
+  to_port           = "3306"
+  protocol          = "tcp"
+ cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web-node.id
 
-#resource "aws_security_group_rule" "Https" {
-#  type              = "ingress"
-#  from_port         = "443"
-#  to_port           = "443"
-#  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "Https" {
+  type              = "ingress"
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web-node.id
+
+
+}
+
+resource "aws_security_group_rule" "NTP" {
+  type              = "ingress"
+  from_port         = "123"
+  to_port           = "123"
+  protocol          = "tcp"
 #  cidr_blocks       = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.web-node.id
-##
-#
-#}
+ security_group_id = aws_security_group.web-node.id
+
+}
 
